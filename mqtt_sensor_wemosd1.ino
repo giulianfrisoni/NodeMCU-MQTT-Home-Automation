445838ef70965b2c3367a828fef35b09bc543c51
giulianfrisoni <36999800+giulianfrisoni@users.noreply.github.com>
2018-04-10 16:22:49 -0600

    Add files via upload

---
+++ mqtt_sensor_wemosd1.ino
@@ -0,0 +1,32 @@
+#include <SimpleDHT.h>
+
+
+
+
+#define sensorpin D8
+SimpleDHT11 dht11;
+void setup() {
+  
+   Serial.begin(115200);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+   Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(sensorpin, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(5000);
+}
